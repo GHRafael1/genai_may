a01c758eca8921700579e60973544586eb2ba302
GHRafael1 <rafael.michaeli@gmail.com>
2025-05-06 10:18:12 +0300

    Add files via upload

---
+++ Day2_streamlit.py
@@ -0,0 +1,33 @@
+import sys
+import streamlit as st
+from streamlit.logger import get_logger
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+
+logger = get_logger(__name__)
+
+import os
+os.environ['HUGGINGFACEHUB_API_TOKEN'] = os.environ['HF_KEY']
+
+
+st.title("my Gen AI App")
+repo_id = "microsoft/Phi-3-mini-4k-instruct"
+temp = 1
+print(repo_id, temp)
+logger.info(f"{temp=}")
+
+
+with st.form("sample_app"):
+    txt = st.text_area("Enter text:", "what GPT stands for?")
+    sub = st.form_submit_button("submit")
+    if sub:
+        llm = HuggingFaceEndpoint(repo_id = repo_id, 
+                                  task="text_generation", 
+                                  temperature=temp
+                                 )
+        chat = ChatHuggingFace(llm = llm, verbose=True)
+
+        logger.info("invoking")
+        ans = chat.invoke(txt)
+        st.info(ans.content)
+        logger.info("Done")
+        
